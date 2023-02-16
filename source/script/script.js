c0939a920b15dbed7cc5f9f5d715ea30b2832637
unknown <moga_denis@yahoo.com>
2023-02-16 16:07:42 +0200

    Added some animations

--- source/script/script.js
+++ source/script/script.js
@@ -1,5 +1,29 @@
-function modifyOpacityWhenScrolling()
+function modifyHeaderWhenScrolling()
 {
     let percentage = 100 - document.documentElement.scrollTop * 0.6;
+    
     document.getElementById("header").style.opacity = String(percentage + '%');
+
+    if (document.documentElement.scrollTop > 100)
+        document.getElementById("header").style.pointerEvents = 'none';
+    else
+        document.getElementById("header").style.pointerEvents = 'all';
+}
+
+function modifyPhotosWhenScrolling()
+{
+    let percentage = 0;
+
+    if (document.documentElement.scrollTop >= 200 && document.documentElement.scrollTop <= 400)
+        percentage = 100;
+    else if (document.documentElement.scrollTop < 200)
+        percentage = (document.documentElement.scrollTop + 15) * 0.5;
+    else if (document.documentElement.scrollTop > 400)
+        percentage = 100 - (document.documentElement.scrollTop - 385) * 0.5;
+        if (percentage < 15)
+            percentage = 15;
+
+    document.getElementById("coding-photo").style.opacity = String(percentage + '%');
+    document.getElementById("car-photo").style.opacity = String(percentage + '%');
+    document.getElementById("football-photo").style.opacity = String(percentage + '%');
 }
